5dd478a2761ef9e20c32b555e942117a8aea7640
codeguruvaa <mohanbvej@gmail.com>
2022-03-15 12:17:33 +0530

    init

---
+++ src/Components/Slider.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Style.css';
+
+function Slider() {
+    return (
+        <div>
+
+            <div className='slider'></div>
+
+
+        </div>
+    );
+}
+
+export default Slider;
